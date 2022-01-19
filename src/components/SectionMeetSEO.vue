c6c89bd4cdc0540643c32f2c25e39b5a7f327911
XanderGale <alessandro.galeazzi@icloud.com>
2022-01-19 19:42:28 +0100

    Completed SectionMeetSEO + SectionTitle components

--- src/components/SectionMeetSEO.vue
+++ src/components/SectionMeetSEO.vue
@@ -3,17 +3,17 @@
         <div class="container">
             <SectionTitle 
                 h2-text="meet the new agency SEO template from the avada team"
-                p-text="sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium"
-                />
+                p-text="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium."
+            />
             <div class="features-container">
                 <div v-for="(feature, index) in templateFeatures" :key="index" class="feature">
-                    <i>{{ feature.icon }}</i>
+                    <i :class="feature.icon"></i>
                     <h3>{{ feature.title }}</h3>
                     <p>{{ feature.paragraph }}</p>
                 </div>
             </div>
 
-            <img src="../assets/images/agency-seo-desk-front-800x380.jpg" alt="Agency SEO desk front image">
+            <img src="../assets/images/agency-seo-desk-front.jpg" alt="Agency SEO desk front image">
         </div>
     </section>
 </template>
@@ -30,19 +30,19 @@ export default {
         return{
             templateFeatures: [
                 {
-                    icon: 'icon',
-                    title: 'Title',
-                    paragraph: 'paragraph1',
+                    icon: 'fas fa-road',
+                    title: 'establish goals',
+                    paragraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.1',
                 },
                 {
-                    icon: 'icon',
-                    title: 'title',
-                    paragraph: 'paragraph2',
+                    icon: 'fas fa-video',
+                    title: 'work with a team',
+                    paragraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.2',
                 },
                 {
-                    icon: 'icon',
-                    title: 'title',
-                    paragraph: 'paragraph3',
+                    icon: 'fas fa-tint',
+                    title: 'get results',
+                    paragraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.3',
                 },
             ]
         }
@@ -74,9 +74,22 @@ export default {
                 box-shadow: -1px -1px 30px 1px rgba(0,0,0,0.1);
 
                 min-height: 250px;
-                margin: 50px 10px;
                 width: calc((100% / 3) - 20px);
+                margin: 50px 10px;
+                padding: 30px;
+                i{
+                    color: $yelloworange;
+                    font-size: 30px;
+                }
+                h3{
+                    margin-top: 30px;
+                    margin-bottom: 15px;
+                    text-transform: capitalize;
+                }
             }
+        }
+        img{
+            width: 100%;
         }
     }
 </style>
